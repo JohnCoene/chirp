9ca5d330ecdb9000779e9301c0f8f60eed5918bd
JohnCoene <jcoenep@gmail.com>
2019-01-31 20:01:41 +0100

    init

---
+++ R/01-init.R
@@ -0,0 +1,28 @@
+#' Initialise
+#'
+#' Create a \code{_chirp.yml} file in working directory.
+#'
+#' @export
+nest <- function(){
+
+  config <- "_chirp.yml"
+
+  if(file.exists(config)){
+    cat(
+      crayon::red(cli::symbol$cross), " The configuration file,", crayon::underline(config), " already exists.\n",
+      sep = ""
+    )
+
+
+  } else {
+    file.copy(
+      system.file("templates/chirp.yml", package = "chirp"),
+      config
+    )
+
+    cat(
+      crayon::green(cli::symbol$tick), "Copied", crayon::underline(config), "\n"
+    )
+  }
+
+}
