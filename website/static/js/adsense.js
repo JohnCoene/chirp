a541bebee4dea6eb2d785fd759c71a2bba3b63cc
JohnCoene <jcoenep@gmail.com>
2019-08-26 13:20:09 +0200

    recreate adsense code

--- website/static/js/adsense.js
+++ website/static/js/adsense.js
@@ -1,4 +1,4 @@
 (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-7425245790017381",
-    enable_page_level_ads: true
+  google_ad_client: "ca-pub-7425245790017381",
+  enable_page_level_ads: true
 });
